f1304f6bec5e08cef2755980f1155c835484bfdc
jeonghwanpang <102663453+jeonghwanpang@users.noreply.github.com>
2023-02-09 06:30:39 +0000

    step1 python

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Hello, world!')
+
+#0생략
+a=-.7
+print(a)
+
+#지수 표현 방식 10의9승
+a = 1e9
+print(a)
+
+a= 75.2e1
+print(a)
+
+#실수 표현 한계 0.899999999
+a = 0.3+0.6
+print(a)
+#소수 4째 자리에서 반올림
+print(round(a,4))
